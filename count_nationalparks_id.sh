5d086332dc929bcc72bf312e2afc03f95930cb74
Andy Townsend <ajtown@ubuntuvm76.atownsend.org.uk>
2022-06-25 07:42:19 +0000

    count_nationalparks_id.sh now deals with different database and filesystem users and must be run from root.

--- count_nationalparks_id.sh
+++ count_nationalparks_id.sh
@@ -2,8 +2,10 @@
 # Count national parks, and see if there are more or fewer that when we last
 # looked.
 # ------------------------------------------------------------------------------
-#PATH=/home/renderaccount/bin:/home/renderaccount/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-mv /home/renderaccount/data/nationalparks_id.justnow /home/renderaccount/data/nationalparks_id.previously
-psql -d gis -c "SELECT distinct osm_id,name FROM planet_osm_polygon WHERE (boundary = 'national_park');" > /home/renderaccount/data/nationalparks_id.justnow
-diff /home/renderaccount/data/nationalparks_id.justnow /home/renderaccount/data/nationalparks_id.previously
+local_filesystem_user=ajtown
+local_renderd_user=_renderd
+#local_renderd_user=_renderd
+mv /home/${local_filesystem_user}/data/nationalparks_id.justnow /home/${local_filesystem_user}/data/nationalparks_id.previously
+sudo -u ${local_renderd_user} psql -d gis -c "SELECT distinct osm_id,name FROM planet_osm_polygon WHERE (boundary = 'national_park');" > /home/${local_filesystem_user}/data/nationalparks_id.justnow 2> /dev/null
+diff /home/${local_filesystem_user}/data/nationalparks_id.justnow /home/${local_filesystem_user}/data/nationalparks_id.previously
 #
