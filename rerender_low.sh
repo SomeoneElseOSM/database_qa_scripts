3aa5aa884cc6983a4868308100bda185311a84a4
Andy Townsend <ajtown@ubuntuvm78.atownsend.org.uk>
2022-08-22 22:30:39 +0000

    Added comment to top of rerender_low.sh.

--- rerender_low.sh
+++ rerender_low.sh
@@ -1,2 +1,8 @@
+# ----------------------------------------------------------------------------
+# rerender_low.sh
+#
+# This uses https://github.com/alx77/render_list_geo.pl .
+# The parameters below rerender zooms 1-12 for the UK and Ireland for "ajt".
+# ----------------------------------------------------------------------------
 PATH=/home/renderaccount/bin:/home/renderaccount/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 /home/renderaccount/src/render_list_geo.pl/render_list_geo.pl -n 1 -z 1 -Z 12 -x -9.5 -X 2.72 -y 49.39 -Y 61.26 -m ajt
