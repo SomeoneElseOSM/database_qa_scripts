d4a8dd915b489e34ea5ddeff51d504a64aacfed3
Andy Townsend <ajtown@ubuntuvm78.atownsend.org.uk>
2022-08-22 21:46:15 +0000

    Updated purge_old_tiles.sh to for 22.04 To work with call_pyosmium.sh.

--- purge_old_tiles.sh
+++ purge_old_tiles.sh
@@ -15,10 +15,9 @@
 # https://github.com/SomeoneElseOSM/mod_tile/tree/zoom2022 .
 #
 # When a tile is marked "dirty" the access time is set to a long time ago
-# (20 years or about 240 months ago in the standard mod_tile; 10k days or 
-# about 27 years or 333 months in my version)
+# (20 years or about 240 months ago in the standard mod_tile).
 # If it is left dirty for 3 months, it'll appear to have been last accessed
-# > 336 months ago.
+# > 243 months ago.
 # Higher zoom tiles that aren't dirtied have to be looked for using more a 
 # recent modification time (3 months).
 # Lower zoom tiles that aren't dirtied likewise, but we're more tolerant of
@@ -27,25 +26,18 @@
 # ----------------------------------------------------------------------------
 # When newly dirtied tiles have been dirty for 3 months, purge them
 # ----------------------------------------------------------------------------
-find /var/lib/mod_tile/ajt -type f ! -newermt `date +%Y%m%d --date="332 month ago"` -exec rm {} \; -print
+find /var/lib/mod_tile/ajt -type f ! -newermt `date +%Y%m%d --date="243 month ago"` -exec rm {} \; -print
 #
 # ----------------------------------------------------------------------------
-# When tiles dirtied at 20 years have been dirty for 3 months, purge them
-# ----------------------------------------------------------------------------
-find /var/lib/mod_tile/ajt -type f ! -newermt `date +%Y%m%d --date="243 month ago"` -newermt `date +%Y%m%d --date="328 month ago"` -exec rm {} \; -print
+# Which metatiles are dirtied by render_expired and which are deleted (or
+# ignored) depends on how render_expired is called from e.g. call_pyosmium.sh.
+# Here we're assuming parameters of "--min-zoom=13 --touch-from=13 --delete-from=19 --max-zoom=20".
 #
-# ----------------------------------------------------------------------------
-# High zoom tiles aren't dirtied; check for 3 months.
-# ----------------------------------------------------------------------------
-find /var/lib/mod_tile/ajt/19 -type f ! -newermt `date +%Y%m%d --date="3 month ago"` -exec rm {} \; -print
-find /var/lib/mod_tile/ajt/2? -type f ! -newermt `date +%Y%m%d --date="3 month ago"` -exec rm {} \; -print
-#
-# ----------------------------------------------------------------------------
-# Low zoom tiles aren't dirtied; just check for 6 months.
+# High zoom tiles are deleted by render_expired so no need to process here.
+# Low zoom tiles aren't dirtied; just check for files over 6 months old.
 # ----------------------------------------------------------------------------
 find /var/lib/mod_tile/ajt/12 -type f ! -newermt `date +%Y%m%d --date="6 month ago"` -exec rm {} \; -print
 find /var/lib/mod_tile/ajt/11 -type f ! -newermt `date +%Y%m%d --date="6 month ago"` -exec rm {} \; -print
 find /var/lib/mod_tile/ajt/10 -type f ! -newermt `date +%Y%m%d --date="6 month ago"` -exec rm {} \; -print
 find /var/lib/mod_tile/ajt/? -type f ! -newermt `date +%Y%m%d --date="6 month ago"` -exec rm {} \; -print
-
 #
