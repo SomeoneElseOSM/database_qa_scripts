e341f77e4e2560607bc60fca5fa70b4bc5cf428a
Andy Townsend <ajtown@map2.atownsend.org.uk>
2022-09-03 07:08:41 +0000

    Start from zoom 3 to avoid render_list_geo errors.

--- rerender_low.sh
+++ rerender_low.sh
@@ -4,5 +4,5 @@
 # This uses https://github.com/alx77/render_list_geo.pl .
 # The parameters below rerender zooms 1-12 for the UK and Ireland for "ajt".
 # ----------------------------------------------------------------------------
-PATH=/home/renderaccount/bin:/home/renderaccount/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-/home/renderaccount/src/render_list_geo.pl/render_list_geo.pl -n 1 -z 1 -Z 12 -x -9.5 -X 2.72 -y 49.39 -Y 61.26 -m ajt
+local_filesystem_user=renderaccount
+/home/${local_filesystem_user}/src/render_list_geo.pl/render_list_geo.pl -n 1 -z 3 -Z 12 -x -9.5 -X 2.72 -y 49.39 -Y 61.26 -m ajt
