00050b4ce8ed71a7232f26df2f970729195be9d3
Andy Townsend <ajtown@dell5000.atownsend.org.uk>
2025-11-29 17:13:50 +0000

    Updated townlands script to send mail to a townlands alias.

--- count_townlands_id.sh
+++ count_townlands_id.sh
@@ -17,9 +17,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
+# 29/11/2025 17:11:
+# Amended script to send output to a local alias "townlands" rather then just
+# relying on cron output.  The "townlands" alias in /etc/aliases will receive mail.
+# -----------------------------------------------------------------------------
 local_filesystem_user=ajtown
 local_renderd_user=_renderd
 mv /home/${local_filesystem_user}/data/townlands_id.justnow /home/${local_filesystem_user}/data/townlands_id.previously
 sudo -u ${local_renderd_user} psql -d gis -c "SELECT distinct osm_id,name FROM planet_osm_polygon WHERE (admin_level = '10') AND (place = 'locality');" > /home/${local_filesystem_user}/data/townlands_id.justnow 2> /dev/null
-diff /home/${local_filesystem_user}/data/townlands_id.previously /home/${local_filesystem_user}/data/townlands_id.justnow
+diff /home/${local_filesystem_user}/data/townlands_id.previously /home/${local_filesystem_user}/data/townlands_id.justnow > /home/${local_filesystem_user}/data/townlands_id.diff
+#
+wclines=`cat /home/${local_filesystem_user}/data/townlands_id.diff | wc -l`;
+if [ $wclines -ne 0 ]
+then
+    cat /home/${local_filesystem_user}/data/townlands_id.diff | mail -s "Change in number or name of townlands" townlands
+fi
 #
